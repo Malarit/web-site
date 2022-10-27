3b01c776c713e128787b983c53488b35d29046a1
Malarit <nuclearre@gmail.com>
2022-10-27 20:47:16 +0300

    getting started on catalog

--- src/components/header/drop.tsx
+++ src/components/header/drop.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import cn from "classnames";
+import { Link } from "react-router-dom";
 
 import style from "./drop.module.scss";
 import appStyle from "../../app.module.scss";
 
-
 const Drop: React.FC<{ active: boolean }> = ({ active }) => {
   return (
     <div className={cn({ [style.root]: true, [style.active]: active })}>
-      <div className={cn(appStyle.container, style.container)}>
-        <div></div>
+      <div className={style.container}>
+        <div className={style.dropBlock}>
+          <ul>
+            <li><Link to="">Молочные</Link></li>
+          </ul>
+        </div>
         <div></div>
       </div>
     </div>
